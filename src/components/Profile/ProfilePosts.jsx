60e8f8979d17f4bc9a83f15cedfb6b29b308b8f9
Indira Pandey <indubhusal@gmail.com>
2024-04-01 21:47:37 -0700

    Added view single post and delete button

--- src/components/Profile/ProfilePosts.jsx
+++ src/components/Profile/ProfilePosts.jsx
@@ -1,37 +1,42 @@
 import React, { useEffect, useState } from "react"
-import { Axios } from "axios"
-import { useParams } from "react-router-dom"
+import axios from "axios"
+import { useParams, Link } from "react-router-dom"
 
 function ProfilePosts() {
+  const { username } = useParams()
   const [isLoading, setIsLoading] = useState(true)
   const [posts, setPosts] = useState([])
-  const { username } = useParams()
 
   useEffect(() => {
     async function fetchPosts() {
       try {
-        const response = await Axios.get(`/profile/${username}/posts`)
+        const response = await axios.get(`/profile/${username}/posts`)
+        console.log(response.data)
         setPosts(response.data)
         setIsLoading(false)
       } catch (e) {
-        console.log("Sorry, something went wrong in posts.")
+        console.log("There was a problem or the request was cancelled.", e)
       }
     }
     fetchPosts()
-  }, [])
+  }, [username])
 
-  //if (isLoading) return <div className="Loading">Loading...</div>
+  if (isLoading) return <div className="Loading">Loading...</div>
+
   return (
-    <div className="profile-container__posts">
-      <p className="profile-container__posts-link">This is a sample post.</p>
-      <div className="profile-container__posts-actions">
-        <span className="edit">
-          <i className="fas fa-edit"></i>
-        </span>
-        <span className="delete">
-          <i className="fas fa-trash-alt"></i>
-        </span>
-      </div>
+    <div>
+      {posts.map(post => {
+        const date = new Date(post.created_at)
+        const dateFormatted = `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`
+        return (
+          <div className="profile-container__posts" key={post.id}>
+            <Link to={`/post/${post.id}`}>
+              <div className="profile-container__posts-link">{post.title}</div>
+            </Link>
+            <span className="profile-container__posts-link"> {dateFormatted} </span>
+          </div>
+        )
+      })}
     </div>
   )
 }
--- src/components/ViewSinglePost/ViewSinglePost.jsx
+++ src/components/ViewSinglePost/ViewSinglePost.jsx
@@ -1,13 +1,49 @@
-import React, { useEffect } from "react"
+import React, { useEffect, useState } from "react"
 import Page from "../Page/Page"
+import axios from "axios"
+import { useParams, useNavigate } from "react-router-dom"
 
 function ViewSinglePost() {
+  const navigate = useNavigate()
+  const { id } = useParams()
+  const [post, setPost] = useState("")
+
+  useEffect(() => {
+    async function fetchPost() {
+      try {
+        const response = await axios.get(`/post/${id}`)
+        setPost(response.data)
+      } catch (e) {
+        console.log("There was a problem or the request was cancelled.", e)
+      }
+    }
+    fetchPost()
+  }, [])
+
+  async function deleteHandler() {
+    const areYouSure = window.confirm("Do you really want to delete this post?")
+    if (areYouSure) {
+      try {
+        const response = await axios.delete(`/post/${id}`, { data: { token: localStorage.getItem("portfolioToken") } })
+        if (response.data == "Success") {
+          navigate(`/profile/${localStorage.getItem("portfolioUsername")}`)
+        }
+      } catch (e) {
+        console.log(e)
+      }
+    }
+  }
   return (
-    <Page title={`This is sample`}>
+    <Page title={post.title}>
       <div className="wrapper wrapper__border">
         <div className="container-home ">
-          <h2>Example Post title</h2>
-          <body>This is a body</body>
+          <div className="profile-container__posts-actions">
+            <a onClick={deleteHandler} className="delete">
+              <i className="fas fa-trash-alt"></i>
+            </a>
+          </div>
+          <h2 className="container-home__header">{post.title}</h2>
+          <div className="container-home__body">{post.body}</div>
         </div>
       </div>
     </Page>
